56bfaeda70752ff87e8fac957b4c4bad59c4ef17
Erik Davidson <edavidson@shoretel.com>
2016-08-29 16:04:58 -0500

    Make more red go away

--- timing_test.go
+++ timing_test.go
@@ -25,3 +25,17 @@ func (s *GomolSuite) TestTestClockNow(c *C) {
 
 	c.Check(clock().Now(), Equals, realNow)
 }
+
+func (s *GomolSuite) TestRealClockNow(c *C) {
+	// This test is completely pointless because it's not something that can really
+	// be tested but I was sick of seeing the red for a lack of a unit test.  So I created
+	// this one and figure even on slow systems the two lines should be executed within
+	// one second of each other. :P
+	setClock(&realClock{})
+
+	timeNow := time.Now()
+	clockNow := clock().Now()
+
+	diff := clockNow.Sub(timeNow)
+	c.Check(diff < time.Second, Equals, true)
+}
